a4e93c9c6a212db8ac14fe11ba8124b5424d9427
Rijal97 <rizaludinbima@gmail.com>
2024-12-09 22:53:07 +0800

    Create BMIQ.py

---
+++ BMIQ.py
@@ -0,0 +1,64 @@
+#Program Bubble Sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print("Bubble Sort:", sorted_arr)
+
+#Program Merge Sort
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    sorted_arr = []
+    while left and right:
+        if left[0] < right[0]:
+            sorted_arr.append(left.pop(0))
+        else:
+            sorted_arr.append(right.pop(0))
+    sorted_arr.extend(left or right)
+    return sorted_arr
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+print("Merge Sort:", sorted_arr)
+
+#Program Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+arr = [12, 11, 13, 5, 6]
+sorted_arr = insertion_sort(arr)
+print("Insertion Sort:", sorted_arr)
+
+#Program Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [10, 7, 8, 9, 1, 5]
+sorted_arr = quick_sort(arr)
+print("Quick Sort:", sorted_arr)
